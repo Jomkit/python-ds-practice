cecd4a0b46f92792076cc00ee9a213d5f977740d
Jomkit <j.jujaroen96@gmail.com>
2023-10-24 09:53:50 +0700

    Figured out how to solve fs_4_reverse_vowels after consulting the internet

--- fs_4_reverse_vowels/reverse_vowels.py
+++ fs_4_reverse_vowels/reverse_vowels.py
@@ -19,3 +19,20 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+    j = 0
+    vowel = [0] * len(s)    #vowel list for storing vowels
+    s = list(s)
+
+    #storing the vowels separately
+    for i in range(len(s)):
+        if s[i] in 'AEIOUaeiou':
+            vowel[j] = s[i]
+            j += 1
+
+    #Placing the vowels in rev order in s
+    for i in range(len(s)):
+        if s[i] in 'AEIOUaeiou':
+            j -= 1
+            s[i] = vowel[j]
+        
+    return ''.join(s)
